e8727f9d0258c7c7439114e85f4d30a536e188ab
Hdzi <taojinhou@qq.com>
2019-01-15 17:27:19 +0800

    返回gradle依赖

--- src/main/java/hdzi/editstarters/gradle/GradleSpringBootEditor.kt
+++ src/main/java/hdzi/editstarters/gradle/GradleSpringBootEditor.kt
@@ -1,14 +1,27 @@
 package hdzi.editstarters.gradle
 
 import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.DataKeys
 import hdzi.editstarters.springboot.SpringBootEditor
+import hdzi.editstarters.springboot.bean.ProjectDependency
 import hdzi.editstarters.springboot.bean.StarterInfo
-import java.util.*
+import org.gradle.tooling.GradleConnector
+import org.gradle.tooling.model.idea.IdeaProject
+import org.gradle.tooling.model.idea.IdeaSingleEntryLibraryDependency
+import java.io.File
 
 /**
  * Created by taojinhou on 2019/1/14.
  */
-class GradleSpringBootEditor(context: DataContext) : SpringBootEditor(context, Collections::emptyList) {
+class GradleSpringBootEditor(context: DataContext) : SpringBootEditor(context, {
+    val basePath = context.getData(DataKeys.PROJECT)?.basePath
+    val connect = GradleConnector.newConnector().forProjectDirectory(File(basePath)).connect()
+    val ideaModule = connect.getModel(IdeaProject::class.java).modules.getAt(0)
+    ideaModule.dependencies.map {
+        val moduleVersion = (it as IdeaSingleEntryLibraryDependency).gradleModuleVersion!!
+        ProjectDependency(moduleVersion.group, moduleVersion.name, moduleVersion.version)
+    }
+}) {
 
     override fun addDependencies(starterInfos: Collection<StarterInfo>) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
