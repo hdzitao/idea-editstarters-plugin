2748959497c934ed1cdc176f59f12d2c9750c6ea
陶锦侯 <taojinhou@021.com>
2022-06-20 17:13:47 +0800

    缓存持久化转json保存版

--- src/main/java/hdzi/editstarters/initializr/chain/cache/CacheComponent.java
+++ src/main/java/hdzi/editstarters/initializr/chain/cache/CacheComponent.java
@@ -1,7 +1,9 @@
 package hdzi.editstarters.initializr.chain.cache;
 
+import com.google.gson.Gson;
 import com.intellij.openapi.components.*;
 import com.intellij.openapi.project.Project;
+import com.intellij.util.xmlb.XmlSerializerUtil;
 import hdzi.editstarters.dependency.SpringBootProject;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -11,20 +13,22 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @State(name = "spring_boot_project_cache",
         storages = @Storage(value = "editstarters.xml", roamingType = RoamingType.DISABLED))
-public class CacheComponent implements PersistentStateComponent<Map<String, SpringBootProject>> {
+public class CacheComponent implements PersistentStateComponent<CacheComponent> {
     public static CacheComponent getInstance(Project project) {
         return ServiceManager.getService(project, CacheComponent.class);
     }
 
+    private final Gson gson = new Gson();
 
-    public final Map<String, SpringBootProject> projects = new ConcurrentHashMap<>();
+
+    public final Map<String, String> projects = new ConcurrentHashMap<>();
 
     public SpringBootProject get(String url, String version) {
-        return this.projects.get(key(url, version));
+        return gson.fromJson(this.projects.get(key(url, version)), SpringBootProject.class);
     }
 
     public void put(String url, String version, SpringBootProject project) {
-        this.projects.put(key(url, version), project);
+        this.projects.put(key(url, version), gson.toJson(project));
     }
 
     private String key(String url, String version) {
@@ -33,12 +37,12 @@ public class CacheComponent implements PersistentStateComponent<Map<String, Spri
 
     @Nullable
     @Override
-    public Map<String, SpringBootProject> getState() {
-        return projects;
+    public CacheComponent getState() {
+        return this;
     }
 
     @Override
-    public void loadState(@NotNull Map<String, SpringBootProject> state) {
-        this.projects.putAll(state);
+    public void loadState(@NotNull CacheComponent component) {
+        XmlSerializerUtil.copyBean(component, this);
     }
 }
