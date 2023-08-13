fcf5ca282e86505fddc08cb7ce307c71531be597
hdzitao <taojinhou@qq.com>
2023-08-13 12:15:38 +0800

    调整ui

--- src/main/java/io/github/hdzitao/editstarters/ui/swing/SelectedTableConstants.java
+++ src/main/java/io/github/hdzitao/editstarters/ui/swing/SelectedTableConstants.java
@@ -7,8 +7,8 @@ package io.github.hdzitao.editstarters.ui.swing;
  */
 public class SelectedTableConstants {
     public static final int COLUMN_MAX = 2;
-    public static final int REMOVE_BUTTON_INDEX = 1;
-    public static final int STARTER_INDEX = 0;
+    public static final int REMOVE_BUTTON_INDEX = 0;
+    public static final int STARTER_INDEX = 1;
 
     public static final int REMOVE_BUTTON_WIDTH = 20;
 }
